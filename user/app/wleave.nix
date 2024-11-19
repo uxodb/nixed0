05efa40f2c9dfaa2099f6b9e9f329b77fb72520e
uxodb <20535246+uxodb@users.noreply.github.com>
2024-11-19 04:59:15 +0100

    feat: add app

---
+++ user/app/wleave.nix
@@ -0,0 +1,10 @@
+{ config, pkgs, inputs, xSettings, ... }:
+
+{
+  home.packages = [
+    pkgs.wleave
+  ];
+  xdg.configFile."wleave" = {
+    source = config.lib.file.mkOutOfStoreSymlink "/home/uxodb/flakes/user/conf/wleave";
+  };
+}
