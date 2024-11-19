390bf9bb4f618bf9f0f72afb0ccd1550fa0fc130
uxodb <20535246+uxodb@users.noreply.github.com>
2024-11-20 00:02:50 +0100

    fix(hyprpaper): change to service

--- user/app/hypr.nix
+++ user/app/hypr.nix
@@ -9,7 +9,11 @@
   programs.hyprlock.enable = true;
   services.hyprpaper.enable = true;
 
-  xdg.configFile."hypr/".source =
-    config.lib.file.mkOutOfStoreSymlink "/home/uxodb/flakes/user/conf/hypr";
+  xdg.configFile = {
+    "hypr/hyprpaper.conf".source = config.lib.file.mkOutOfStoreSymlink "/home/uxodb/flakes/user/conf/hypr/hyprpaper.conf";
+    "hypr/hyprland.conf".source = config.lib.file.mkOutOfStoreSymlink "/home/uxodb/flakes/user/conf/hypr/hyprland.conf";
+    "hypr/hyprpaper.conf".source = config.lib.file.mkOutOfStoreSymlink "/home/uxodb/flakes/user/conf/hypr/hyprpaper.conf";
+    "hypr/wallpapers/".source = config.lib.file.mkOutOfStoreSymlink "/home/uxodb/flakes/user/conf/hypr/wallpapers";
+  };
 }
 
