9bfc04b4ebc6be9adbe9da3e8228b82b048eea90
uxodb <20535246+uxodb@users.noreply.github.com>
2024-11-23 05:38:35 +0100

    feat(shell): add prep options to build shell

--- shell/build.nix
+++ shell/build.nix
@@ -9,12 +9,48 @@ mkShell {
   ];
 
   shellHook = ''
-    mkdir /home/uxodb/.ssh
-    mkdir -p /home/uxodb/.config/sops/age
     echo "##############################################################"
     echo "#### Entered shell with cmake, git, sops and home-manager ####"
     echo "#### --                                                   ####"
-    echo "#### Remember age keys.txt                                ####"
+    echo "#### \"menu\" to enter menu                                 ####"
     echo "##############################################################"
+    
+    menu() {
+    	while true; do
+		clear
+    		echo "################ Menu ################"
+		echo "## 1. Create SSH folder            ###"
+		echo "## 2. Create SOPS folder and file  ###"
+		echo "## 3. Copy hardware configuration  ###"
+		echo "## 4. exit                         ###"
+		echo "######################################"
+
+		read -p "Choose option [1-4]: " choice
+	
+		case "$choice" in
+			1) if mkdir ~/.ssh; then
+				echo "SSH folder created."
+			else
+				echo "Failed to create SSH folder."
+			fi;;
+			2) if mkdir -p ~/.config/sops/age && touch ~/.config/sops/age/keys.txt; then
+				echo "SOPS file prep succeeded."
+			else
+				echo "SOPS file prep failed."
+			fi;;
+			3) nix_hw="/etc/nixos/hardware-configuration.nix"
+			0_hw="~/nixed0/host/hardware.nix"
+			if cp "$nix_hw" "$0_hw"; then
+				echo "Hardware config copied."
+			else
+				echo "Hardware config copy failed."
+			fi;;
+			4) echo "Exit"
+			break;;
+			*) echo "Invalid [1-4]";;
+		esac
+		read -p "Enter to return"
+	    done
+    }
   '';
 }
