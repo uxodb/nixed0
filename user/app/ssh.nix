f0eb67fc0a744bb4248a5bb09d8d66e72bd60d7e
uxodb <20535246+uxodb@users.noreply.github.com>
2024-11-24 03:35:17 +0100

    feat(apps): add to ssh config

--- user/app/ssh.nix
+++ user/app/ssh.nix
@@ -2,6 +2,7 @@
 
   programs.ssh = {
     enable = true;
+    addKeysToAgent = "yes";
     serverAliveInterval = 60;
     serverAliveCountMax = 6;
     extraConfig = builtins.readFile /run/secrets/sshconfig;
