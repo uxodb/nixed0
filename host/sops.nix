1a8c2019cc3f8c7e11dba72d7a0ea6232762b197
uxodb <20535246+uxodb@users.noreply.github.com>
2024-11-24 03:34:49 +0100

    refactor(conf): use with expression for sops.nix

--- host/sops.nix
+++ host/sops.nix
@@ -1,22 +1,24 @@
-{ config, inputs, xSettings, ... }:
+{ config, inputs, xSettings,  ... }:
 
 {
-  sops = {
+  sops = with xSettings; {
     defaultSopsFile = ../secrets.json;
-    age.keyFile = "/home/${xSettings.username}/.config/sops/age/keys.txt";
+    age.keyFile = "/home/${username}/.config/sops/age/keys.txt";
     # age.sshKeyPaths = [ "/home/uxodb/nixed0_id_ed25519" ];
     secrets = {
       github_id_ed25519 = {
-        owner = xSettings.username;
-	path = "/home/${xSettings.username}/.ssh/github_id_ed25519";
+        owner = username;
+	path = "/home/${username}/.ssh/github_id_ed25519";
       };
       named0_id_rsa = {
-        owner = xSettings.username;
-        path = "/home/${xSettings.username}/.ssh/named0_id_rsa";
+        owner = username;
+        path = "/home/${username}/.ssh/named0_id_rsa";
       };
-      nixed0_id_ed25519 = {mode = "0444";}; # age key convert
+      nixed0_id_ed25519 = {  # age key convert
+        mode = "0444";
+      };
       sshconfig = {
-        owner = xSettings.username;
+        owner = username;
       };
       userpasswd = {
         neededForUsers = true;
