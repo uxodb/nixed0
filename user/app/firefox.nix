de13148d1495e4628b093aebc8631ded739f532e
uxodb <20535246+uxodb@users.noreply.github.com>
2024-11-27 07:15:03 +0100

    fix(apps): fix firefox profile path, add symlink

--- user/app/firefox.nix
+++ user/app/firefox.nix
@@ -6,7 +6,7 @@
       id = 0;
       name = "personal";
       isDefault = true;
-      path = "${xdg.configHome}/firefox/personal/";
+      path = "personal";
     };
     policies = {
       DefaultDownloadDirectory = "${home.homeDirectory}/Downloads";
@@ -29,8 +29,18 @@
       };
     };
   };
-  xdg.configFile."firefox/" = with xSettings; {
-    source = config.lib.file.mkOutOfStoreSymlink "${appConfig}/firefox";
+  home.file = with xSettings; {
+    ".mozilla/firefox/personal/chrome" = {
+      recursive = true;
+      source = config.lib.file.mkOutOfStoreSymlink "${appConfig}/firefox/personal/chrome";
+    };
+    ".mozilla/firefox/personal/user.js" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${appConfig}/firefox/personal/user.js";
+    };
   };
 
+  xdg.configFile."firefox/" = with config; {
+    recursive = true;
+    source = config.lib.file.mkOutOfStoreSymlink "${home.homeDirectory}/.mozilla";
+  };
 }
