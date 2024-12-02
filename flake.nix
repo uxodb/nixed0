fb6eca7aecfd1fffc7cd8e707bdd32afc7377177
uxodb <20535246+uxodb@users.noreply.github.com>
2024-12-02 22:07:54 +0100

    fix(flake): revert last commit

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
         locale = "en_US.UTF-8";
         username = "uxodb";
         hostname = "nixed0";
-        appConfig = "${self}/user/conf";
+        appConfig = "/home/uxodb/nixed0/user/conf";
       };
       xpkgs = nixpkgs.legacyPackages.${xSettings.system};
     in
