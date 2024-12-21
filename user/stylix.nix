6682ce3381aed32c6787fcc8116af3426f05f212
uxodb <20535246+uxodb@users.noreply.github.com>
2024-12-22 00:37:04 +0100

    feat(conf): add stylix font sizes

--- user/stylix.nix
+++ user/stylix.nix
@@ -7,6 +7,11 @@
     image = ../assets/wallpapers/wp.png;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     fonts = {
+      sizes = {
+        desktop = 10;
+        popups = 12;
+        terminal = 12;
+      };
       sansSerif = {
         name = "FantasqueSansM Nerd Font";
         package = pkgs.nerd-fonts.fantasque-sans-mono;
