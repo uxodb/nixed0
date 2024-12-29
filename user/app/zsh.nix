7be40788e140fd802a92355041e447a24e8920d7
uxodb <20535246+uxodb@users.noreply.github.com>
2024-12-29 03:12:06 +0100

    feat(apps): add aliases to zsh for eza

--- user/app/zsh.nix
+++ user/app/zsh.nix
@@ -7,6 +7,17 @@
     enableCompletion = true;
     defaultKeymap = "emacs";
     syntaxHighlighting.enable = true;
+    shellAliases = {
+      ls = "eza --color=always --group-directories-first --icons";
+      ll = "eza -la --icons --octal-permissions --group-directories-first";
+      l = "eza -bGF --header --git --color=always --group-directories-first --icons";
+      llm = "eza -lbGd --header --git --sort=modified --color=always --group-directories-first --icons";
+      la = "eza --long --all --group --group-directories-first";
+      lx = "eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale --color=always --group-directories-first --icons";
+      lS = "eza -1 --color=always --group-directories-first --icons";
+      lt = "eza --tree --level=2 --color=always --group-directories-first --icons";
+      "l." = "eza -a | grep -E '^\.'";
+    };
     initExtra = ''
       fastfetch
     '';
