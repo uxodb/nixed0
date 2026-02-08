110fd89a48ef502109b11b4bd7c6f37f469370d3
uxodb <20535246+uxodb@users.noreply.github.com>
2026-02-08 18:01:59 +0100

    chore(user): rm unused apps and configs

--- user/conf/waybar/scripts/swaync.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-COUNT=$(swaync-client -In)
-ENABLED=
-DISABLED=
-if [ $COUNT != 0 ]; then DISABLED="$COUNT  "; fi
-if swaync-client -D | grep -q "true" ; then echo $ENABLED; else echo $DISABLED; fi
