28e90cc3141e7510aa2b9187b13133669bd3ee54
uxodb <20535246+uxodb@users.noreply.github.com>
2026-02-12 19:09:37 +0100

    fix(nixos): replace deprecated system with stdenv.hostPlatform.system

--- nixos/services/solaar.nix
+++ nixos/services/solaar.nix
@@ -1,7 +1,7 @@
 { config, pkgs, inputs, ... }: {
   services.solaar = {
     enable = true;
-    package = inputs.solaar.packages.${pkgs.system}.default;
+    package = inputs.solaar.packages.${pkgs.stdenv.hostPlatform.system}.default;
     window = "hide";
     batteryIcons = "regular";
     extraArgs = "";
