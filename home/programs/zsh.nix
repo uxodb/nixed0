9c22611704ee95eabff94ac60dca97d44b938c7b
uxodb <20535246+uxodb@users.noreply.github.com>
2026-02-14 01:07:00 +0100

    feat(programs): expand zsh configuration: dirHashes, setOpt, history, etc

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -4,19 +4,57 @@
   lib,
   xSettings,
   ...
-}: {
+}: let
+  inherit (config.home) homeDirectory;
+in {
   programs.zsh = {
     enable = true;
-    dotDir = config.xdg.configHome;
+    completionInit = "autoload -Uz compinit && compinit";
+    dotDir = "${config.xdg.configHome}/zsh";
     autosuggestion.enable = true;
     enableCompletion = true;
     defaultKeymap = "emacs";
+    dirHashes = {
+      docs = "${homeDirectory}/Documents";
+      dl = "${homeDirectory}/Downloads";
+      pics = "${homeDirectory}/Pictures";
+      vids = "${homeDirectory}/Videos";
+      games = "${homeDirectory}/Games";
+      proj = "${homeDirectory}/Projects";
+      ss = "${homeDirectory}/Screenshots";
+      casts = "${homeDirectory}/Screencasts";
+    };
+    setOptions =
+      [
+        "NO_BEEP"
+        "NOMATCH"
+        "NOTIFY"
+        "NO_EXTENDEDGLOB"
+        "PUSHD_TO_HOME"
+        "GLOB_DOTS"
+        "SHORT_LOOPS"
+        "CHECK_JOBS"
+        "C_BASES"
+      ];
     syntaxHighlighting.enable = true;
     shellAliases = {
       # eza.nix
       # fzf.nix
       # fd.nix
     };
-    initContent = "fastfetch";
+    initContent = ''
+      fastfetch
+    '';
+    history = {
+      path = "${config.programs.zsh.dotDir}/.zsh_history";
+      append = false;
+      findNoDups = false;
+      extended = true;
+      ignoreSpace = true;
+      save = 10000;
+      share = true;
+      size = 10000;
+
+    };
   };
 }
