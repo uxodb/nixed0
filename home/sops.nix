4c59287db02759cadaa8f0c030f9e7bc7572daf9
uxodb <20535246+uxodb@users.noreply.github.com>
2026-02-20 17:49:31 +0100

    fix(secrets): remove paths in favor of default

--- home/sops.nix
+++ home/sops.nix
@@ -7,18 +7,10 @@ in{
     defaultSopsFile = ./secrets.json;
     age.keyFile = "${homeDir}/.config/sops/age/keys.txt";
     secrets = {
-      vps_host = {
-        # path = "%r/vps_hostname.txt";
-      };
-      lsw_host = {
-        # path = "%r/lsw_hostname.txt";
-      };
-      lsw_user = {
-        # path = "%r/lsw_user.txt";
-      };
-      lsw_port = {
-        # path = "%r/lsw_port.txt";
-      };
+      vps_host = {};
+      lsw_host = {};
+      lsw_user = {};
+      lsw_port = {};
     };
   };
 }
